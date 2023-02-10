1a13935fc3a1d4c7c6213a80a0007a32520f7a1c
João Vitor Polizelli <jvitorpzl@gmail.com>
2023-02-09 22:19:54 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barbearia</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+    <main class="site-inteiro">
+    <header>
+        <h1 class="titulo">Barber Shop</h1>
+</header>
+    
+    <nav class="menu">
+    <ul>
+
+<li>Home</li>
+<li>Serviços</li>
+<li>Contato</li>
+<li>Agende Agora</li>
+
+</ul>
+</nav>
+    </main>
+</body>
+</html>
