1a39736b9bbf5a5ec8e8957283ffb1a632d9ac28
João Vitor Polizelli <jvitorpzl@gmail.com>
2023-03-22 22:20:49 -0300

    inserindo container

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Barbearia</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
@@ -51,7 +52,38 @@
 <nav class="navbar navbar-expand-lg" style="background-color: black; border-bottom: 10px solid black;">
 </nav>
 
-<img src="" class="img-fluid" alt="...">
+<div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/imagem-barbeiro-topo.png" height="750" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/imagem-barbeiro-topo2.png" height="750" class="d-block w-100" alt="...">
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+</div>
+
+<nav class="navbar navbar-expand-lg" style="background-color: black; border-bottom: 70px solid black;">
+</nav>
+
+
+  
+<h2 class="font text-center" style="font-size: 90px">
+<img src="img/imagem-barbeiro-qmsomos.avif" class="quemsomos">
+<span class="color">QUEM SOMOS</span>
+</h2>
+
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
